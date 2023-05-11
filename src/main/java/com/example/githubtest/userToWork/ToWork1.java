3bf2b7976c17fbdad0410680a0ac31fa29fbe6cb
root <hello@itcast.cn>
2023-05-11 22:48:39 +0800

    githubTest

--- src/main/java/com/example/githubtest/userToWork/ToWork1.java
+++ src/main/java/com/example/githubtest/userToWork/ToWork1.java
@@ -12,17 +12,23 @@ import java.util.stream.Stream;
 public class ToWork1 {
     public static void main(String[] args) {
         List<PojoTest> list = new ArrayList<>();
+
         PojoTest pojo1 = new PojoTest();
         pojo1.setTest1("A");
         pojo1.setTest2(Arrays.asList("1", "2"));
         PojoTest pojo2 = new PojoTest();
         pojo2.setTest1("B");
         pojo2.setTest2(Collections.emptyList());
+        PojoTest pojo3 = new PojoTest();
+        pojo3.setTest1("C");
+        pojo3.setTest2(Arrays.asList("1", "2"));
+
         list.add(pojo1);
         list.add(pojo2);
+        list.add(pojo3);
 
         String result = list.stream()
-                .flatMap(pojo -> pojo.getTest2().isEmpty() ? Stream.of("") : pojo.getTest2().stream()
+                .flatMap(pojo -> pojo.getTest2().isEmpty() ? Stream.of(String.format("%s|", pojo.getTest1())) : pojo.getTest2().stream()
                         .map(test2Value -> String.format("%s|%s", pojo.getTest1(), test2Value)))
                 .collect(Collectors.joining(","));
 
