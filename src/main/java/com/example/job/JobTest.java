0d00a7beb21470ad429155a0fa012a76b9f3763d
root <hello@itcast.cn>
2023-06-18 23:08:04 +0800

    githubTest

--- src/main/java/com/example/job/JobTest.java
+++ src/main/java/com/example/job/JobTest.java
@@ -4,10 +4,18 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Objects;
 
 public class JobTest implements Job {
+
+    public static final String YYYY_MM_DD_HH_MM_SS_SSS = "yyyy-MM-dd HH:mm:ss.sss";
+    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+    public static final String YYYY_MM_DD_WITH_SLASH = "yyyy/MM/dd";
+    public static final String YYYY_MM_DD_WITH_STRIKE = "yyyy-MM-dd";
+
     @Override
     public void execute(JobExecutionContext jobContext) throws JobExecutionException {
         // 目的：获取当前调度前一个小时的数据，oprDate >= startDate and oprDate < EndDate
@@ -23,5 +31,14 @@ public class JobTest implements Job {
         Date jobNowDate = jobContext.getFireTime();
         // 设置下？上？次框架调度时间
 //        jobContext.setResult();
+
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("格式化前:" + now);
+        String format = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+        System.out.println("格式化后:" + format);
+
+        LocalDateTime parse = LocalDateTime.parse(format, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+        System.out.println("反格式化:" + parse);
     }
 }
